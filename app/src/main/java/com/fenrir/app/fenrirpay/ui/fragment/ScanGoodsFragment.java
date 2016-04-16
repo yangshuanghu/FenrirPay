054eaad59924330ba9435a66af8000594bf60aae
xuemaotang <xuemao.tang@fenrir-inc.com.cn>
2016-04-16 20:00:27 +0800

    自定义扫码控件：由于Camera只支持部分尺寸，因此需要自定义

--- app/src/main/java/com/fenrir/app/fenrirpay/ui/fragment/ScanGoodsFragment.java
+++ app/src/main/java/com/fenrir/app/fenrirpay/ui/fragment/ScanGoodsFragment.java
@@ -1,14 +1,11 @@
 package com.fenrir.app.fenrirpay.ui.fragment;
 
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import butterknife.ButterKnife;
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
-import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
+import android.widget.TextView;
 
 import com.fenrir.app.fenrirpay.R;
 import com.fenrir.app.fenrirpay.ui.AppComponent;
@@ -17,23 +14,34 @@ import com.fenrir.app.fenrirpay.ui.component.ScanGoodsComponent;
 import com.fenrir.app.fenrirpay.ui.module.ScanGoodsModule;
 import com.fenrir.app.fenrirpay.ui.presenter.ScanGoodsPresenter;
 import com.fenrir.app.fenrirpay.util.LogUtil;
+import com.fenrir.app.fenrirpay.view.barcode.ZXingScannerView;
 import com.google.zxing.Result;
 
-import java.util.concurrent.TimeUnit;
-
 import javax.inject.Inject;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by DaggerGenerator on 2016/04/16.
  */
-public class ScanGoodsFragment extends BaseFragment{
+public class ScanGoodsFragment extends BaseFragment {
     @Inject
     ScanGoodsPresenter presenter;
 
-    private ZXingScannerView scannerView;
+    @Bind(R.id.goods_scanner_view)
+    ZXingScannerView scannerView;
+    @Bind(R.id.bar_code_text_view)
+    TextView barCodeTextView;
+    @Bind(R.id.format_text_view)
+    TextView formatTextView;
+
     private ZXingScannerView.ResultHandler resultHandler = new ZXingScannerView.ResultHandler() {
         @Override
         public void handleResult(Result result) {
+            barCodeTextView.setText(result.getText());
+            formatTextView.setText(result.getBarcodeFormat().toString());
+
             LogUtil.m(this, "getText:", result.getText());
             LogUtil.m(this, "getBarcodeFormat:", result.getBarcodeFormat().toString());
 
@@ -42,19 +50,18 @@ public class ScanGoodsFragment extends BaseFragment{
     };
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-//        View view = inflater.inflate(R.layout.scan_goods_fragment, container, false);
-//        ButterKnife.bind(this, view);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.scan_goods_fragment, container, false);
+        ButterKnife.bind(this, view);
 
-        scannerView = new ZXingScannerView(getActivity());
 
-        return scannerView;
+        return view;
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState){
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-            //init view at here.
+        //init view at here.
     }
 
     @Override
@@ -73,17 +80,17 @@ public class ScanGoodsFragment extends BaseFragment{
     }
 
     @Override
-    public void inject(AppComponent appComponent){
+    public void inject(AppComponent appComponent) {
         ScanGoodsComponent scanGoodsComponent = DaggerScanGoodsComponent.builder()
-            .appComponent(appComponent)
-            .scanGoodsModule(new ScanGoodsModule(this))
-            .build();
+                .appComponent(appComponent)
+                .scanGoodsModule(new ScanGoodsModule(this))
+                .build();
         scanGoodsComponent.injectFragment(this);
         scanGoodsComponent.injectPresenter(presenter);
     }
 
     @Override
-    public void onDestroyView(){
+    public void onDestroyView() {
         super.onDestroyView();
         ButterKnife.unbind(this);
     }
